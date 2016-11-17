7c35224b3c81727079f2203f324eb9966a4f6b86
BoliviaBigby <rigo1_15@uni.worc.ac.uk>
2016-11-17 14:39:54 +0000

    first

---
+++ www/js/index.js
@@ -0,0 +1,12 @@
+<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
+$(document).on("pagecreate","#pageone",function(){
+  
+  	$('#tapholdtext').on("taphold",function random() {
+	return !Math.round(Math.random());
+}
+{
+    	
+
+
+});
