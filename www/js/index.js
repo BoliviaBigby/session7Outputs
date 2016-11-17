ba3f67bde49e06facfb68a217b322faa954d63ec
BoliviaBigby <rigo1_15@uni.worc.ac.uk>
2016-11-17 15:25:07 +0000

    removed comments

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,7 @@
-<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
 
 $(document).on("pagecreate","#pageone",function(){
   
-  	$('#tapholdtext').on("ontap",function random() {
+  	$('#ontap').on("ontap",function random() {
 	return !Math.round(Math.random());
 }
 {
